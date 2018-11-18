e17649ccd6401f76643c99b21ac98bbec97a5894
A-l-i-s-a <i.ivlieva2011@yandex.ru>
2018-11-19 00:00:33 +0300

    Initial commit

---
+++ learning_logs/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Topic(models.Model):
+    """Тема которую изучает пользователль"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING, )
+
+    def __str__(self):
+        return self.text
+
+
+class Entry(models.Model):
+    """Информация, изученная пользователем по теме"""
+    topic = models.ForeignKey('Topic', on_delete=models.DO_NOTHING,)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Возврвщает строковое представление мделию"""
+        if len(self.text) > 50:
+            return self.text[:50] + "..."
+        return self.text[:50]
